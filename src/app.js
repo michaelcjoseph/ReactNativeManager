24ac52dc75e5e901699d9a2bb121df01c826608d
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-06-01 18:00:00 -0700

    Set up firebase

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import reducers from './reducers';
 
-const App = () => (
-  <Provider store={createStore(reducers)}>
-    <View>
-      <Text>Manager App</Text>
-    </View>
-  </Provider>
-);
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCkxpHaIEoaaXm8DvxBCZuBDOA3Li9joT8',
+      authDomain: 'reactnativemanager-ab450.firebaseapp.com',
+      databaseURL: 'https://reactnativemanager-ab450.firebaseio.com',
+      projectId: 'reactnativemanager-ab450',
+      storageBucket: 'reactnativemanager-ab450.appspot.com',
+      messagingSenderId: '605407966920',
+    };
+
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Manager App</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
 
 export default App;
