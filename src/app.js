24153535abc8dd20a22e971404e4b6dd7154fb07
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-06-01 17:49:43 -0700

    Set up redux for app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { View, Text } from 'react-native';
+import reducers from './reducers';
 
-const App = () => {
-  return (
+const App = () => (
+  <Provider store={createStore(reducers)}>
     <View>
       <Text>Manager App</Text>
     </View>
-  );
-};
+  </Provider>
+);
 
 export default App;
