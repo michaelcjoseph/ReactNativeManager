08c1f3fb9211415b2f73ed7a061ff6a7786834b9
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-06-02 17:41:45 -0700

    Add holder for employee creation form with nav

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -10,7 +11,19 @@ const RouterComponent = () => {
         <Scene key="login" component={LoginForm} title="Please Login" initial />
       </Scene>
       <Scene key="main">
-        <Scene key="employeeList" component={EmployeeList} title="Employee List" />
+        <Scene
+          initial
+          key="employeeList"
+          component={EmployeeList}
+          title="Employee List"
+          rightTitle="Add"
+          onRight={() => Actions.employeeCreate()}
+        />
+        <Scene
+          key="employeeCreate"
+          component={EmployeeCreate}
+          title="Create Employee"
+        />
       </Scene>
     </Router>
   );
